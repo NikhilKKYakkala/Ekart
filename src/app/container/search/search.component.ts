cf616ab6ed27c410bb65c11df1eed8e224228fdd
Nikhil Yakkala <nikhilyakkala@gmail.com>
2024-04-04 00:23:56 +0100

    completed ContentChild

--- src/app/container/search/search.component.ts
+++ src/app/container/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -11,11 +11,14 @@ export class SearchComponent {
   @Output()
   searchTextChanged: EventEmitter<string> = new EventEmitter<string>();
 
+  @ViewChild('searchInput') searchInputEl: ElementRef
+
   onSearchTextChanged(){
-    this.searchTextChanged.emit(this.searchText)
+    
   }
 
-  updateSearch(event: any) {
-    this.searchText = event.target.value;
+  updateSearchText() {
+    this.searchText = this.searchInputEl.nativeElement.value;
+    this.searchTextChanged.emit(this.searchText)
   }
 }
